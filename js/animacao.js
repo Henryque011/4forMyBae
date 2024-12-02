9d26e20afde353538404fd609bcf36e977a2dcca
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2024-12-02 00:15:05 -0300

    upt

--- js/animacao.js
+++ js/animacao.js
@@ -8,3 +8,28 @@ $('.carrousel').slick({
     autoplay: true,
     autoplaySpeed: 3000
 });
+
+// contador
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const startDate = new Date("2023-10-03T00:00:00");
+
+    function updateTimer() {
+        const now = new Date();
+        const elapsedTime = now - startDate;
+
+        const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((elapsedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+
+        document.getElementById("days").textContent = days;
+        document.getElementById("hours").textContent = hours;
+        document.getElementById("minutes").textContent = minutes;
+        document.getElementById("seconds").textContent = seconds;
+    }
+
+    updateTimer();
+    setInterval(updateTimer, 1000);
+});
+
