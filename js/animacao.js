0c20629065f37cddcf13c5fa1330b5535bf939d6
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-03-18 13:10:12 -0300

    Commit final.

--- js/animacao.js
+++ js/animacao.js
@@ -15,14 +15,47 @@ $('.carrousel').slick({
 });
 
 
-// contador
-// script.js
+// // contador
+// // script.js
+// document.addEventListener("DOMContentLoaded", function () {
+//     const startDate = new Date("2023-10-03T00:00:00");
+
+//     function updateTimer() {
+//         const now = new Date();
+//         const elapsedTime = now - startDate;
+
+//         const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
+//         const hours = Math.floor((elapsedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//         const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+//         const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+
+//         document.getElementById("days").textContent = days;
+//         document.getElementById("hours").textContent = hours;
+//         document.getElementById("minutes").textContent = minutes;
+//         document.getElementById("seconds").textContent = seconds;
+//     }
+
+//     updateTimer();
+//     setInterval(updateTimer, 1000);
+// });
+
+// FIm do que eu acreditei que fosse paras sempre. n;
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    const startDate = new Date("2023-10-03T00:00:00");
+    const startDate = new Date("2023-10-03T19:00:00");
+    const endDate = new Date("2025-03-15T23:59:59");
+    let interval;
 
     function updateTimer() {
         const now = new Date();
-        const elapsedTime = now - startDate;
+        let elapsedTime = now - startDate;
+        let remainingTime = endDate - now;
+
+        if (remainingTime <= 0) {
+            clearInterval(interval);
+            elapsedTime = endDate - startDate;
+        }
 
         const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
         const hours = Math.floor((elapsedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -36,6 +69,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     updateTimer();
-    setInterval(updateTimer, 1000);
+    interval = setInterval(updateTimer, 1000);
 });
-
